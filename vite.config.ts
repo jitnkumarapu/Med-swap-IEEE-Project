c2c32f969ba21748b67eba4c48e64bb61736ac5c
jitnkumarapu <jitinkumarapu@gmail.com>
2025-09-22 22:15:48 +0530

    deployed

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Med-swap-Ieee-Project-/",
   server: {
     host: "::",
     port: 8080,
